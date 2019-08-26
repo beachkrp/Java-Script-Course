f16bb838f223a24c3e7ea2305170ca53d1102246
Kevin Pearson <krp@cswnet.com>
2019-08-26 15:53:59 -0500

    first section of lecture

---
+++ Basics/variables_and_numbers.js
@@ -0,0 +1,21 @@
+//Create numeric variable
+//Node.js 5.12.0
+var num1 = 8;
+
+//output
+console.log("The number in the num1 variable is " +  num1);
+
+
+//Mini challenge
+
+//Create second variable 
+
+var num2 = 98;
+
+
+//output
+console.log( num2*num1 - num1/num2 + num2/num1);
+//Different output
+console.log("The numbers concatenated: "+num2+num1);
+//casting 
+console.log("The number added together: " + (num2 + num1));
