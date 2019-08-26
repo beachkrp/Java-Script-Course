8e36f5bd17046b490ad0f63088f13b1e2e4e4666
Kevin Pearson <krp@cswnet.com>
2019-08-26 16:13:34 -0500

    add Challenge #3

--- Basics/variables_and_numbers.js
+++ Basics/variables_and_numbers.js
@@ -35,7 +35,7 @@ console.log("The result is "  + calculate);
 //Challenge  #1
 //  Convert Fahrenheit to Celsuius
 
-
+//create varible
 var temp = 95;
 
 //Convert Fahrenheit to Celsius   C = (F-32)*5/9
@@ -44,9 +44,22 @@ console.log("The Temperature " + temp + " in Fahrenheit is " + ((temp-32)*5/9) +
 //Challenge #2  
 // Convert Celsuis to Fahrenheit
 
-
+// Reassign variable 
 temp = 20;
 // Conversion  F = C*9/5 + 32
 console.log("The Temperature " + temp + " in Celsuis is " + ((temp*9/5) +32) + "  in Celsuis");
+
+
+//Challenge #3
+//Convert mile to kilometers   1 mile = 1.609344 km
+
+//
+const conversion = 1.609344;
+var miles = 4.2;
+
+//output 
+console.log(miles + " miles is the same as " + miles*conversion + " kilometers");
+
+
 
 
