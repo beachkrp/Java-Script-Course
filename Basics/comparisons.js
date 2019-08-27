53752fdcf919909c8926a7bb5089fafd8d34cc12
Kevin Pearson <krp@cswnet.com>
2019-08-27 18:42:32 -0500

    First comparison

---
+++ Basics/comparisons.js
@@ -0,0 +1,14 @@
+//Create two variables for comparison
+
+var num1 = 15;
+
+var num2 = 15;
+
+//Variable to test for equality
+
+var isEqual;
+
+isEqual = num1 == num2;
+
+//output
+console.log(isEqual);
