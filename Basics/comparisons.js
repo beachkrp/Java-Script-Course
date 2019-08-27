180dab16cb0e5ed4248e355ab1df829225922e26
Kevin Pearson <krp@cswnet.com>
2019-08-27 18:51:21 -0500

    adding inequal comparisons

--- Basics/comparisons.js
+++ Basics/comparisons.js
@@ -12,3 +12,30 @@ isEqual = num1 !== num2;
 
 //output
 console.log(isEqual);
+
+// another variable for testing
+var isGreater = num1>num2;
+
+//output 
+console.log(isGreater);
+
+//Change to greater than ir equal 
+
+isGreater = num1 >= num2;
+
+//output
+
+console.log(isGreater);
+
+//Another comparison variable
+
+var isLessThan = num1 < num2;
+
+//output
+
+console.log(isLessThan);
+
+isLessThan = num1 <= num2;
+
+//output 
+console.log(isLessThan);
