ca1a93c4b4f7384ec7efeb3dffded002a23338fb
Kevin Pearson <krp@cswnet.com>
2019-08-29 12:32:51 -0500

    demonstrating the OR operator

--- Flow/if.js
+++ Flow/if.js
@@ -27,7 +27,29 @@ else if(num ==50) {
 	//Output 
 	console.log("The number is 50");
 }
+else if (num >45) {
+	//Output
+	console.log("the Number is greater than 45");
+}
+
 else {
 	//Output
 	console.log("The condition is false");
 }
+
+
+//Demonstrating the OR operator
+
+//Assign  a mew variable
+
+var num2 = 25;
+
+//if statement
+if ((num >45)  || (num2 < 25)) {
+	//Output
+	console.log("One of the conditions is true");
+}
+else {
+	console.log("None of the conditions are true");
+}
+
