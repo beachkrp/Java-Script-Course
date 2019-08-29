3f21e753ad13fc3b1330bbef9dc9e083f15c2707
Kevin Pearson <krp@cswnet.com>
2019-08-29 12:16:45 -0500

    Inital setup of lecture

---
+++ Flow/if.js
@@ -0,0 +1,11 @@
+//Declare a boolean variable  node.js 5.12
+var test = true;
+
+//if structure
+if (true) {
+	//output
+	console.log("The condition is ture");
+}
+
+//output after teh if structure
+console.log("This is the code after the if strucruer");
