e646a638d52768fca78e2a0b2990bae9247469f9
Kevin Pearson <krp@cswnet.com>
2019-08-30 20:47:13 -0500

    using logical operators in switch

--- Flow/switch.js
+++ Flow/switch.js
@@ -4,25 +4,28 @@ var num = 50;
 var state = "The number is ";
 
 //switch statement
- switch(num){
+ switch(true){
  	// Execute if num is 1
- 	case 1:
+ 	case num == 1:
  		console.log(state + "1");
  	break;
  	//Execute if num is 2
- 	case 2:
+ 	case num == 2:
  		console.log(state + "2");
  	break;
  	//Execute if num is 3
- 	case 3:
+ 	case  num ==3:
  		console.log(state + "3");
  	break;
  	//Execute is num is 4
- 	case 4:
+ 	case  num == 4:
  		console.log(state + "4");
+ 	break;
+ 	case num > 40:
+ 		console.log("The number is greater than 40");
  	break;
  	//Default
  	default:
- 		console.log("'num'  has a value not covered by the swithc statement");
+ 		console.log("'num'  has a value not covered by the switch statement");
 
  }
