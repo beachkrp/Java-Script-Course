c466be9a52d51b34511926704f743aff79300927
Kevin Pearson <krp@cswnet.com>
2019-08-30 20:43:28 -0500

    initial switch structure

---
+++ Flow/switch.js
@@ -0,0 +1,28 @@
+//Variable to call number 
+
+var num = 50;
+var state = "The number is ";
+
+//switch statement
+ switch(num){
+ 	// Execute if num is 1
+ 	case 1:
+ 		console.log(state + "1");
+ 	break;
+ 	//Execute if num is 2
+ 	case 2:
+ 		console.log(state + "2");
+ 	break;
+ 	//Execute if num is 3
+ 	case 3:
+ 		console.log(state + "3");
+ 	break;
+ 	//Execute is num is 4
+ 	case 4:
+ 		console.log(state + "4");
+ 	break;
+ 	//Default
+ 	default:
+ 		console.log("'num'  has a value not covered by the swithc statement");
+
+ }
