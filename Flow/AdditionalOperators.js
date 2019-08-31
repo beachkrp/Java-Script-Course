8dd7fcf6f5eea270be6247ba4361799be6d4558f
Kevin Pearson <krp@cswnet.com>
2019-08-30 21:18:07 -0500

    modulus operator

---
+++ Flow/AdditionalOperators.js
@@ -0,0 +1,19 @@
+//Set two new variables
+
+var num1 = 17;
+var num2 = 3;
+
+//Output remainder
+console.log(num1%num2);
+
+//mini challegne
+if(num1%2 == 0)
+	console.log(num1 + " is even");
+else 
+	console.log(num1 + " is odd");
+
+//second way
+if(num1%2 == 1)
+	console.log(num1 +" is odd");
+else
+	console.log(num1 +" is even");
