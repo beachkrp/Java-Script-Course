013ab65422fe0841531a0b393544e20940288530
Kevin Pearson <krp@cswnet.com>
2019-09-01 13:15:44 -0500

    nested for loops

--- Flow/for.js
+++ Flow/for.js
@@ -3,3 +3,12 @@ for (var i = 5; i >= 1; i--) {
 	//output
 	console.log("This is loop # " + i);
 }
+//nested for loops
+for (var i = 1; i <= 5; i++) {
+	for (var j  = 0;  j<= 3 ;j ++) {
+		//Inner loop output
+		console.log("The inner loop has run " + j + " times")
+	}
+	//Outer loop output
+	console.log("The outer loop has run " + i + " times");
+}
