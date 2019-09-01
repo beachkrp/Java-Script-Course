5abd5d3ca902cc267bb6ed8588a43604c2ba3314
Kevin Pearson <krp@cswnet.com>
2019-09-01 14:27:11 -0500

    challenge

--- Flow/for.js
+++ Flow/for.js
@@ -31,3 +31,41 @@ for (var i = 0; i < 5; i++) {
 	console.log("This is loop # " + i)
 }
 
+//Create a for loop to test scope
+
+for (var i = 0; i < 5; i++) {
+	var test = 0;
+	test++;
+	console.log(test)
+}
+	
+
+
+//Challenge
+//Create a loopt that runs from 1 to 10
+// Multiply the value of the counter by 5 adding to the last steps
+//Output the total sum at the end
+//create a variable to hold the sums
+
+var sum = 0;
+
+for (var i = 1; i <= 10; i++) {
+	//Output of the multiple by 5 of the counter and add 
+	sum += i*5;
+	console.log("In the loop " + i + " the value of five times the counter is " + sum);
+}
+console.log("\nThe final sum is " + sum+"\n\n");
+
+//Challenge 2
+//Repeat Skip the loops 4 and 7
+sum = 0;
+
+for (var i = 1; i <= 10; i++) {
+	//Output of the multiple by 5 of the counter and add 
+	//skip iterations 4 and 7
+	if ((i==4) || (i==7))
+		{continue;}
+	sum += i*5;
+	console.log("In the loop " + i + " the value of five times the counter is " + sum);
+}
+console.log("\nThe final sum is " + sum + "\n\n" );
