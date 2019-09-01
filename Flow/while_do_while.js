c285d29ce28131561b11afdcf98858be9c1f4303
Kevin Pearson <krp@cswnet.com>
2019-09-01 14:39:54 -0500

    do while loop

--- Flow/while_do_while.js
+++ Flow/while_do_while.js
@@ -13,3 +13,16 @@ while(true){
 	//increment the counter
 	count++;
 }
+//reset test and count
+test = false;
+count = 0;
+//do while loop
+do {
+	if (count == 3) {
+		test==false;
+		break;
+	}
+	console.log("The count is " + count);
+	//increment the counter
+	count++;
+}while(test)
