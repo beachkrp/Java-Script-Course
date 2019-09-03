83aaf4f15d661f12e4f4ced257c0c97a9fe4329d
Kevin Pearson <krp@cswnet.com>
2019-09-02 21:34:59 -0500

    initial creation of function

---
+++ Functions/functions.js
@@ -0,0 +1,14 @@
+//Create a function called sum
+function sum() {
+	//Create three variables
+	var num1 = 2;
+	var num2 = 3;
+	var sum = num1 + num2;
+	//Output
+	console.log("The total of " + num1 + " and " + num2 + " is " + sum);
+	
+}
+
+//Call the function
+
+sum();
