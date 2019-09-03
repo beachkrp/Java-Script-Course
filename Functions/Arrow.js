1e15a0f568751f2804e338e546e38a54b74ad89e
Kevin Pearson <krp@cswnet.com>
2019-09-02 22:35:24 -0500

    arrow function with single input parameters

--- Functions/Arrow.js
+++ Functions/Arrow.js
@@ -1,6 +1,5 @@
 //Create a function to add two numbers
 
-var addNumbers = (num1) =>	console.log("The sum of " + num1+" and 9 is  "+ (num1 + 8));
+var addNumbers = num1 =>	console.log("The sum of " + num1+" and 9 is  "+ (num1 + 8));
 
 addNumbers(5);
-
