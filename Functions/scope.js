ad0ebe848bfa9473943a3c32c2299dd394a11651
Kevin Pearson <krp@cswnet.com>
2019-09-03 21:54:49 -0500

    initial set up for scope

---
+++ Functions/scope.js
@@ -0,0 +1,14 @@
+//Create a variable 
+var firstName = "Jane";
+
+
+//Create a declared function
+
+function welcome() {
+	//output
+	console.log("Welcome " + firstName + " to variable scope");
+}
+
+
+//call the function
+welcome()
