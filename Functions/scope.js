eb5ec2572897621b1d59dbac920e1735cdc757f6
Kevin Pearson <krp@cswnet.com>
2019-09-03 22:05:34 -0500

    change in scope

--- Functions/scope.js
+++ Functions/scope.js
@@ -5,10 +5,36 @@ var firstName = "Jane";
 //Create a declared function
 
 function welcome() {
+	// Create variable
+	var lastName = "Smith"
 	//output
 	console.log("Welcome " + firstName + " to variable scope");
 }
 
 
+
 //call the function
 welcome()
+
+//create a second function
+
+// function hello(){
+// 	//Output
+// 	console.log("Hello" + firstName  + " " + lastName);
+// }
+
+//hello();
+
+for (var i = 0; i < 10; i++) {
+	
+}
+
+function greeting() {
+  var givenName = "Jane";
+  if(true){
+  	var surname = "Smith";
+  	console.log("Hello "+ givenName +" "+ surname);
+  }
+}
+
+greeting();
