cd59f4c7f1b65ed5e32bc00150db57f5e121b2f1
Kevin Pearson <krp@cswnet.com>
2019-09-05 18:20:46 -0500

    Intial Array basics

---
+++ Arrays/Array_Basic.js
@@ -0,0 +1,31 @@
+//Create an array
+
+var numArray =[3, 7, 6];
+
+//Give the length of the array
+
+console.log(numArray.length);
+
+//output specific element
+
+console.log(numArray [1]);
+
+//output whole array
+
+console.log(numArray);
+
+//Change element value
+numArray[2] = 50;
+
+//See output of result
+console.log(numArray);
+
+//Assign an element that doesn't exist
+
+numArray[5]  = 50;
+console.log(numArray);
+
+//View an element tha doesn't exist
+
+console.log(numArray[4]);
+
