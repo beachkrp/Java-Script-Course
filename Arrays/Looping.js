63333293ba961cb478c7db876359f4e8da0d24dd
Kevin Pearson <krp@cswnet.com>
2019-09-05 19:04:00 -0500

    challenge

--- Arrays/Looping.js
+++ Arrays/Looping.js
@@ -12,3 +12,21 @@ for (var i = 0; i < shoppingList.length; i++) {
 shoppingList.forEach(function (argument, index) {
 	console.log(index + " " + argument);
 });
+
+//Challenge
+
+//Create an array of cities 
+
+var citiesArray = ["Boston", "Houston", "Tulsa", "Seattle" ];
+
+//loop through the array
+
+citiesArray.forEach(function(element, index){
+	console.log("City number " + (index +1) + " is " + element );
+	})
+
+
+for (var city of citiesArray){
+	console.log(city);
+
+}
