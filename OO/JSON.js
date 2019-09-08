11c990e62bd905fc6da48ad0c6754681d206a0e5
Kevin Pearson <krp@cswnet.com>
2019-09-08 08:46:44 -0500

    addinga JSON object and a function as a property

--- OO/JSON.js
+++ OO/JSON.js
@@ -3,6 +3,15 @@ var lamp = {
 	"type" : "desk",
 	"brightnessOfBulb": 60,
 	"turnedOn": true,
+	manufacturer : {
+		name  : "Big Electtic Company",
+		model : "ABC12",
+		locations : ['USA', "Germany", "UK"]
+	},
+	turnLampOnOrOff : function(){console.log("The lamp will turn on or off")}
 };
 
-console.log(lamp.type)
+console.log(lamp.type);
+console.log(lamp.manufacturer.model);
+
+lamp.turnLampOnOrOff();
