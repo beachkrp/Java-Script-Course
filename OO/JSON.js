9fbc33ad1af838ed9be84d8211432de298cd759b
Kevin Pearson <krp@cswnet.com>
2019-09-08 09:20:40 -0500

    JSON object challenge

--- OO/JSON.js
+++ OO/JSON.js
@@ -8,14 +8,27 @@ var lamp = {
 		model : "ABC12",
 		locations : ['USA', "Germany", "UK"]
 	},
+	//Function to turn lamp on and off
 	turnLampOnOrOff : function(){
-				console.log("The lamp will turn on or off")
+		//Toggle the lamp status
+				if (this.turnedOn)
+					this.turnedOn = false;
+				else
+					this.turnedOn = true;
 						},
 	//Create function to declare what type of lamp we have
 	lampReport : function (argument) {
 		console.log("This lamp is a " + this.type + " lamp");
-	
-}				
+
+	},
+
+	lampStatus : function(){
+		var status = (this.turnedOn ? "on" :"off"); 
+
+		console.log("The lamp is " + status);
+
+
+	}				
 
 };
 
@@ -23,10 +36,20 @@ lamp.type = "overhead";
 console.log(lamp.type);
 console.log(lamp.manufacturer.model);
 
+//Challenge
+
+
 lamp.turnLampOnOrOff();
+
 
 console.log(typeof lamp.brightnessOfBulb);
 console.log(typeof lamp.manufacturer);
 
 
 lamp.lampReport();
+lamp.lampStatus();
+
+//Check to see if it toggles. 
+
+lamp.turnLampOnOrOff();
+lamp.lampStatus();
