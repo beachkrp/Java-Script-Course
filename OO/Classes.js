be9ebdc0ad4a030fd11d98f8bf967cf69b4751ff
Kevin Pearson <krp@cswnet.com>
2019-09-09 00:51:38 -0500

    extending class

--- OO/Classes.js
+++ OO/Classes.js
@@ -1,16 +1,30 @@
 //use strict mode for node 5
 "use strict"; 
 class Person{
-	constructor(){
-		this.firstName = "Mark";
-		this.lastName = "Jones";
+	constructor(first, last){
+		this.firstName = first;
+		this.lastName = last;
 	}
 	//method 
-	hello(){ console.log("Hello " + this.firstName + " from the object");
-
+	hello(){ 
+		console.log("Hello " + this.firstName );
 	}
 }
 
-let person = new Person();
+//Instatiate a class
+let person = new Person("Mark", "Jones");
 
 person.hello();
+
+//Inherited class
+class Student extends Person(){
+	constructor(first, last, major){
+		super(first, last);
+		this.major = major;
+	}
+hello(){
+	console.log("Hello" + this.firstName + "your major is " + this.major);
+}}
+
+let student = new Student("Terry", "Gilliam", "Theatre");
+student.hello();
