007dd7b27b50bb38787dd28771cd9506c1603682
Kevin Pearson <krp@cswnet.com>
2019-10-03 07:15:19 -0500

    adding files to be upgraded

---
+++ Arrays/arrayfunctions.js
@@ -0,0 +1,33 @@
+var cities =  ["Tulsa", "London", "Paris"];
+/*
+push()
+pop()
+unshift()
+shift()
+indexOf()
+splice()
+slice()
+sort()
+map()
+filter()
+*/
+
+
+
+var numbers = [3,5,14,86,11,0];
+
+console.log(numbers.map(function (num) {
+	// body...
+	return num*3;
+}));
+/*
+console.log(numbers.filter(function(number){
+	return number >= 25;
+}))
+
+ console.log(cities.filter(function(c) {
+ 	return c =='Paris';
+ }));
+
+
+*/
---
+++ Basics/variables.js
@@ -0,0 +1,22 @@
+// This is a single line comment
+/*
+This is a multi-line comment
+yes it is 
+*/
+
+// First greeting  variable 
+// let in ECMA 2017 does not work with my system.
+
+var myGreeting = "Hello fron JS!!! "; // "let" does not work on my system.
+
+//Create the output
+
+
+var firstName;
+firstName = 'Kathy';
+
+console.log("hello"+firstName);
+
+console.log("CONGRATULATIONS JOHN SMITH YOU HAVE SOLVED YOUR FIRST JAVASCIPT CHALLENGE")
+
+
