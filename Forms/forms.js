9f4475929bc08de1e7e68e74e75f3068a12003ac
Kevin <krp@cswnet.com>
2019-10-08 14:50:55 +0200

    submit event

--- Forms/forms.js
+++ Forms/forms.js
@@ -2,3 +2,8 @@ document.querySelector("#someInput").addEventListener("input", function (effect)
 
 				console.log(effect.target.value);
 				})
+
+document.querySelector("#aForm").addEventListener("submit", function (argument) {
+		argument.preventDefault();
+		console.log(argument.target.elements.firstName.value);
+})
