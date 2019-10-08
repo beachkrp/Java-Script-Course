d8a82cfba62642256069562a96b1d37a25ef5a8c
Kevin <krp@cswnet.com>
2019-10-08 15:09:02 +0200

    checkboxes and select

--- Forms/forms.js
+++ Forms/forms.js
@@ -6,4 +6,10 @@ document.querySelector("#someInput").addEventListener("input", function (effect)
 document.querySelector("#aForm").addEventListener("submit", function (argument) {
 		argument.preventDefault();
 		console.log(argument.target.elements.firstName.value);
+		if(argument.target.elements.email.checked){
+			console.log("The email was checked");	
+		}
+
+		console.log(argument.target.elements.cities.value)
 })
+
