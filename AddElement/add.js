954fcb66243530c62a9fb24c680ec075be79cdc4
Kevin <krp@cswnet.com>
2019-10-08 08:56:58 +0200

    adding elements to HTML

---
+++ AddElement/add.js
@@ -0,0 +1,15 @@
+//  Create a variable
+let p1 = document.createElement("p");
+
+p1.textContent = "This was created with Javascript";
+
+// append the element to the document opject
+document.querySelector("body").appendChild(p1);
+
+//Create another variave
+
+let p2 = document.createElement("p");
+
+p2.textContent = "This is the second paragraph";
+
+document.querySelector("body").appendChild(p2);
