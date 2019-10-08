b870c4ad45fa5c080b7ae4368189a3885bed15bb
Kevin <krp@cswnet.com>
2019-10-08 11:14:45 +0200

    Using event object

--- Events/event.js
+++ Events/event.js
@@ -1,15 +1,46 @@
+//Select the heading
+let heading1 = document.querySelector("#heading1");
+
+
+//Add an event listener to the heading
+heading1.addEventListener("mouseover", handleMouse);
+
+// Define the event handler
+function handleMouse() {
+
+	let head = document.createElement("h1");
+	head.textContent = 'this is a new header';
+	document.querySelector("#heading1").after(head);
+	heading1.removeEventListener("mouseover",handleMouse);
+}
+
 //Create a variable for the button element
 
-let button1 = document.querySelector("button");
-
+let button1 = document.querySelector("#button1");
+let button2 =  document.querySelector("#button2");
 //Add an event Listener
 button1.addEventListener("click", handleEvent);
+button2.addEventListener("click", handleEvent);
 
-function handleEvent(){
 
-	let p1 = document.createElement("p");
+function handleEvent(event){
+
 	
-	p1.textContent = "The Button was Clicked";
+	let p1 = document.createElement("p");
+
+
+	if(event.target.id == 'button1'){
+
+		p1.textContent = "Button One was Clicked";	
+	}else 
+	{
+		p1.textContent = "Button Two was Clicked";	
+	}
 	document.querySelector("body").appendChild(p1);
-	button1.removeEventListener("click", handleEvent);
+	
+
 }
+
+
+
+
