0c34977659cb70336a0b50881aaf935b07886448
Kevin <krp@cswnet.com>
2019-10-08 09:04:08 +0200

    inserting headings

--- AddElement/add.js
+++ AddElement/add.js
@@ -2,6 +2,8 @@
 let p1 = document.createElement("p");
 
 p1.textContent = "This was created with Javascript";
+//set the id for the element
+p1.setAttribute("id", "para1");
 
 // append the element to the document opject
 document.querySelector("body").appendChild(p1);
@@ -11,5 +13,15 @@ document.querySelector("body").appendChild(p1);
 let p2 = document.createElement("p");
 
 p2.textContent = "This is the second paragraph";
+//set the id for the element
+p2.setAttribute("id", "para2");
 
 document.querySelector("body").appendChild(p2);
+
+//Create a variable for a heading
+let heading = document.createElement("h1");
+//add content
+heading.textContent = "This is a new heading";
+heading.setAttribute("id","heading1");
+
+document.querySelector('#para1').after(heading);
