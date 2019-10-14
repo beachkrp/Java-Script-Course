e93e19e99a35cfef87e39d6669b810522d904a64
Kevin <krp@cswnet.com>
2019-10-14 10:47:05 +0200

    Adding Polymorphism exercises

---
+++ Polymorphism/Polymorphism.js
@@ -0,0 +1,92 @@
+"use strict"
+class Movie{
+	constructor(title) {
+		this.title = title;
+	}
+
+
+	plot(){
+		return "No plot here";
+	}
+
+	get name(){
+		return this.title
+	}
+
+}
+
+class ET extends Movie{
+	constructor(){
+		super("ET");
+	}	
+
+	 plot(){
+	 	return "You know the plot";
+	 }
+
+	}
+
+class StarWars extends Movie{
+	constructor(){
+		super("Star Wars");
+	}	
+
+	 plot(){
+	 	return "You know the plot too";
+	 }
+
+	}
+
+
+
+class AwfulMovies extends Movie{
+
+
+	constructor(){
+		super("Awful Movies");
+
+	}
+
+	
+	plot(){
+
+		return "vomit";		
+	}
+}
+
+class Jurassic extends Movie{
+
+
+	constructor(){
+		super("Jurassic");
+
+	}
+	plot() {
+		return "Dino";
+	}
+}
+
+function selectMovie(){
+	var randomNumber = Math.floor(4*Math.random()) +1;
+	
+	switch (randomNumber){
+		case 1:
+			return new ET();
+		break;
+		case 2:
+			return new StarWars();
+		break;
+		case 3:
+			return new Jurassic();
+		break;
+		case 4:
+			return new AwfulMovies();
+		break;
+	}
+}
+
+for (var i = 0; i < 10; i++) {
+	var movie = selectMovie();
+	console.log("Movie" + movie.name +"is  about" + movie.plot());
+}
+
---
+++ Polymorphism/PolymorphismChallenge.js
@@ -0,0 +1,144 @@
+"use strict";
+// Create a class 
+class Auto{
+	//create a constructor that defines properties for model and cyliners
+	constructor(_model, _cylinder){
+		this.model = _model;
+		this.cylinder = _cylinder;
+	}
+
+
+	
+
+	get modelName(){
+		return this.model;
+	}
+
+	get cylinderNo(){
+		return this.cylinder;
+	}
+
+
+
+	start(){
+
+		console.log("We called start");
+	}
+
+	accelerate()
+
+	{
+		console.log("We called accelerate");
+	}
+
+	brake(){
+
+		console.log("We called brake");
+	}
+
+}
+
+
+class Honda extends Auto{
+
+	constructor(cylinder){
+		super("Honda", cylinder);
+	}
+
+	accelerate(){
+		console.log("Accelerating the " +this.model);
+	}
+
+	start(){
+		console.log("Starting the " + this.model)
+
+	}
+
+	brake(){
+
+		console.log("Braking the " + this.model)
+	}
+
+
+}
+
+
+class Toyota extends Auto{
+
+	constructor(cylinder){
+		super("Toyota", cylinder);
+	}
+
+	accelerate(){
+		console.log("Accelerating the " +this.model);
+	}
+
+	start(){
+		console.log("Starting the " + this.model)
+
+	}
+
+	brake(){
+
+		console.log("Braking the " + this.model)
+	}
+
+
+}
+
+
+class Yugo extends Auto{
+
+	constructor(cylinder){
+		super("Yugo", cylinder);
+	}
+
+	accelerate(){
+		console.log("Accelerating the " +this.model);
+	}
+
+	start(){
+		console.log("Starting the " + this.model)
+
+	}
+
+	brake(){
+
+		console.log("Braking the " + this.model)
+	}
+
+
+}
+
+
+
+
+class REO extends Auto{
+
+	constructor(cylinder){
+		super("REO", cylinder);
+	}
+
+	accelerate(){
+		console.log("Accelerating the " +this.model);
+	}
+
+	start(){
+		console.log("Starting the " + this.model)
+
+	}
+
+	brake(){
+
+		console.log("Braking the " + this.model)
+	}
+
+
+}
+
+
+let yugo = new Yugo(4);
+console.log(yugo.modelName)
+yugo.brake()
+console.log(yugo.cylinderNo)
+console.log(yugo.model)
